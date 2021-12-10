66294f90e0da08690076ab591d276be07e56f029
Supachai Wongmoon <zupaazhai@gmail.com>
2021-12-10 21:00:55 +0700

    add love horo

--- Readme.md
+++ Readme.md
@@ -12,3 +12,23 @@ msg = f'Hello {name}'
 ```python
 x = 0 if x < 1 else x 
 ```
+
+### Play with String
+```python
+
+# lower case
+"message".lower()
+
+# count "a" in string
+"how many a in strin".count('a')
+```
+
+### Get value list from dict
+```python
+dict.values()
+```
+
+### Sum number in list
+```python
+sum([1, 2, 3])
+```
---
+++ day3/love_horo.py
@@ -0,0 +1,35 @@
+'''
+Calculate love score you and your love
+'''
+
+first_name = input('Enter your name: ').lower()
+second_name = input('Enter second name: ').lower()
+combined_name = first_name + second_name
+
+scores = {
+    't': combined_name.count('t'),
+    'r': combined_name.count('r'),
+    'u': combined_name.count('u'),
+    'e': combined_name.count('e'),
+    'l': combined_name.count('l'),
+    'o': combined_name.count('o'),
+    'v': combined_name.count('v'),
+}
+
+total_score = 0
+
+'''
+For loop solution
+'''
+for key, score in scores.items():
+    total_score += score
+
+'''
+sum() solution
+'''
+total_sum_sol = sum(list(scores.values()))
+
+print(f"Your love score is : {total_score}")
+print(f"Your love score sum by list : {total_sum_sol}")
+# print(true_score, love_score)
+
