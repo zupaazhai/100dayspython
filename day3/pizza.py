9b94f3008a1a0dfed425deb688c82b0af57161df
Supachai Wongmoon <zupaazhai@gmail.com>
2021-12-10 20:20:43 +0700

    add short-if`

--- day3/pizza.py
+++ day3/pizza.py
@@ -6,9 +6,26 @@ print('===========================')
 print('=  Welcome to Daddy Pizza =')
 print('===========================')
 
+is_opened = True
+
+if not is_opened:
+    print('Daddy pizza is closed')
+    print('-- Bye --')
+    exit(0)
+
 size = input('Enter your size (S, M, L): ').lower()
 add_pepperoni = input('Add Pepperoni ( $2 for S, $3 for M and L )? (y, n): ').lower()
 add_extra_cheese = input('Add extra cheese ( $1 )? (y, n): ').lower()
+
+'''
+Handle with short-if
+It seems more readable 
+I love it
+
+x = 0 if x < 0 else x
+'''
+add_pepperoni = 'n' if add_pepperoni not in ['y', 'n'] else add_pepperoni
+add_extra_cheese = 'n' if add_extra_cheese not in ['y', 'n'] else add_extra_cheese
 
 total = 0
 
@@ -28,6 +45,10 @@ prices = {
     'm': 20,
     'l': 25
 }
+
+if size not in prices :
+    print("Operation failed !!")
+    exit(0)
 
 total += prices[size]
 
